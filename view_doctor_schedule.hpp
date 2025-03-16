687f0c8d4a635f869e10c6707f0fa6d1108b8a04
Tolstograeva Viktoria <argosi.sanria@gmail.com>
2025-03-16 08:38:50 +0300

    Delete view_doctor_schedule.hpp

--- view_doctor_schedule.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef JUNIOR_ADMIN_SCHEDULE_HPP_
-#define JUNIOR_ADMIN_SCHEDULE_HPP_
-
-#include "database.hpp"
-
-// отображение расписания в личном кабинете младшего админа
-void junior_admin_schedule(database_handler &db, int junior_admin_id);
-
-#endif  // JUNIOR_ADMIN_SCHEDULE_HPP_
