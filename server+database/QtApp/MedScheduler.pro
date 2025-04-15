9fca98dda9ef52f9e63f4b42691e6510ca743caf
VictoriaGrudtsyna <vika.grudtsyna.2006@mail.ru>
2025-04-15 23:57:15 +0300

    update of half the functions

---
+++ server+database/QtApp/MedScheduler.pro
@@ -0,0 +1,62 @@
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+CONFIG += c++17
+
+# You can make your code fail to compile if it uses deprecated APIs.
+# In order to do so, uncomment the following line.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+SOURCES += \
+    src/doctorwindow.cpp \
+    src/homepage.cpp \
+    src/junioradminwindow.cpp \
+    src/main.cpp \
+    src/mainwindow.cpp \
+    src/registrationwindow.cpp \
+    src/senioradminwindow.cpp \
+    src/utils.cpp \
+    ../client/src/client_auth.cpp \
+    ../client/src/client_doctor.cpp \
+    ../client/src/client_junior_admin.cpp \
+    ../client/src/client_patient.cpp \
+    ../client/src/client_senior_admin.cpp \
+    ../client/src/common_for_all.cpp
+
+HEADERS += \
+    include/doctorwindow.h \
+    include/homepage.h \
+    include/junioradminwindow.h \
+    include/mainwindow.h \
+    include/registrationwindow.h \
+    include/senioradminwindow.h \
+    include/utils.h \
+    ../client/include/client_auth.hpp \
+    ../client/include/client_doctor.hpp \
+    ../client/include/client_junior_admin.hpp \
+    ../client/include/client_patient.hpp \
+    ../client/include/client_senior_admin.hpp \
+    ../client/include/common_for_all.hpp
+
+FORMS += \
+    forms/doctorwindow.ui \
+    forms/homepage.ui \
+    forms/junioradminwindow.ui \
+    forms/mainwindow.ui \
+    forms/registrationwindow.ui \
+    forms/senioradminwindow.ui
+
+# Default rules for deployment.
+qnx: target.path = /tmp/$${TARGET}/bin
+else: unix:!android: target.path = /opt/$${TARGET}/bin
+!isEmpty(target.path): INSTALLS += target
+
+RESOURCES += \
+    res.qrc
+
+LIBS += -lcurl
+
+INCLUDEPATH += $$PWD/server+database/client/include
+INCLUDEPATH += $$PWD/include
+
---
+++ server+database/QtApp/src/homepage.cpp
@@ -0,0 +1,114 @@
+#include "homepage.h"
+#include "ui_homepage.h"
+#include <QString>
+#include <QJsonObject>
+#include <QJsonObject>
+#include <QFile>
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <nlohmann/json.hpp>
+#include "server+database/client/include/common_for_all.hpp"
+#include <curl/curl.h>
+#include <nlohmann/json.hpp>
+homepage::homepage(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::homepage)
+{
+    ui->setupUi(this);
+    ui->stackedWidget->setCurrentWidget(ui->appointments_page);
+}
+
+homepage::~homepage()
+{
+    delete ui;
+}
+
+void homepage::on_appointments_button_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->appointments_page);
+}
+
+
+void homepage::on_profile_button_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->profile_page);
+}
+
+
+void homepage::on_doctors_button_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->doctors_page);
+}
+
+
+void homepage::on_hospitals_button_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->hospitals_page);
+}
+
+
+void homepage::on_notifications_button_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->notifications_page);
+}
+
+
+void homepage::on_settings_button_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(ui->settings_page);
+}
+
+
+void homepage::on_view_schedule_button_clicked()
+{
+    ui->view_schedule_error_label->setText("");
+    QString region=ui->region_enter->text();
+    QString settlement_type=ui->settlement_type_enter->text();
+    QString settlement_name=ui->settlement_name_enter->text();
+    QString specialty = ui->specialty_enter->text();
+    QString hospital_id = ui->hospital_id_enter->text();
+    QString doctor_id = ui->doctor_id_enter->text();
+    QJsonObject doctor_data={
+        {"region", region},
+        {"settlement_type", settlement_type},
+        {"settlement_name", settlement_name},
+        {"specialty", specialty},
+        {"hospital_id", hospital_id},
+        {"doctor_id",doctor_id}
+        };
+    QByteArray jsonData = QJsonDocument(doctor_data).toJson();
+    std::string jsonString = jsonData.toStdString();
+    nlohmann::json j = nlohmann::json::parse(jsonString);
+    std::string response = send_post_request("http://localhost:8080/view_doctor_schedule_for_patient", j);
+    nlohmann::json schedule = nlohmann::json::parse(response);
+    QWidget* containerWidget = new QWidget();
+    QVBoxLayout* containerLayout = new QVBoxLayout(containerWidget);
+    for (const auto &entry : schedule["schedule"]) {
+        QWidget* entryWidget = new QWidget();
+        QHBoxLayout* entryLayout = new QHBoxLayout(entryWidget);
+
+        // Добавляем информацию о записи
+        QLabel* dateLabel = new QLabel(QString::fromStdString("Date: " + entry["appointment_date"].get<std::string>()));
+        QLabel* timeLabel = new QLabel(QString::fromStdString("Time: " + entry["appointment_time"].get<std::string>()));
+        QLabel* cabinetLabel = new QLabel(QString::fromStdString("Cabinet: " + entry["cabinet_number"].get<std::string>()));
+
+        // Добавляем элементы в layout виджета записи
+        entryLayout->addWidget(dateLabel);
+        entryLayout->addWidget(timeLabel);
+        entryLayout->addWidget(cabinetLabel);
+
+        // Добавляем виджет записи в контейнер
+        containerLayout->addWidget(entryWidget);
+
+        // Добавляем разделитель (опционально)
+        containerLayout->addWidget(new QFrame());
+        containerLayout->widget()->setObjectName("separator");
+        containerLayout->widget()->setStyleSheet("QFrame { border: 1px solid gray; }");
+    }
+
+    // Устанавливаем контейнер в QScrollArea
+    ui->doctor_schedule->setWidget(containerWidget);
+    ui->doctor_schedule->setWidgetResizable(true);
+    }
+
+
