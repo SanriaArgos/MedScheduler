3b26fa868e19d99558a596b3b99db5872e7cf8ea
VictoriaGrudtsyna <vika.grudtsyna.2006@mail.ru>
2025-04-20 22:56:33 +0300

    delete: unnecessary files

--- senior_admin_menu.cpp
+++
@@ -1,9 +0,0 @@
-#include "senior_admin_menu.hpp"
-#include "system_init.hpp"
-#include <libpq-fe.h>
-#include <iostream>
-
-bool senior_admin_menu(const nlohmann::json &data) {
-    std::cerr << "senior_admin_menu: client-side menu removed. Use JSON interface instead.\n";
-    return false;
-}
