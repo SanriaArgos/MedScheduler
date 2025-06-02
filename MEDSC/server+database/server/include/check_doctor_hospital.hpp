0e495cc21f692a5cad12a87040436093fdd32a18
VictoriaGrudtsyna <vika.grudtsyna.2006@mail.ru>
2025-06-02 13:33:59 +0300

    udtate changelock

--- MEDSC/server+database/server/include/check_doctor_hospital.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef CHECK_DOCTOR_HOSPITAL_HPP_
-#define CHECK_DOCTOR_HOSPITAL_HPP_
-
-// Проверяем, привязан ли доктор к больнице млададмина
-bool check_doctor_hospital(int doctor_id, int admin_hospital_id);
-
-#endif  // CHECK_DOCTOR_HOSPITAL_HPP_
--- MEDSC/server+database/sql/00-init-cron.sql
+++
@@ -1,2 +0,0 @@
-\connect medscheduler
-CREATE EXTENSION IF NOT EXISTS pg_cron;
