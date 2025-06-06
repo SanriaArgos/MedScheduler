a823d8d06e2e33d9a73154aea12e5a9de000e7b2
aurhwwh <balzievaur@gmail.com>
2025-06-06 14:31:30 +0300

    add: patient profile edit in interface

--- QtApp/include/homepage.h
+++ QtApp/include/homepage.h
@@ -44,6 +44,8 @@ private slots:
 
     void on_edit_profile_button_clicked();
 
+    void on_apply_changes_edit_clicked();
+
 private:
     void create_doctor_card(const Doctor &doctor, QVBoxLayout *layout);
     Ui::homepage *ui;
